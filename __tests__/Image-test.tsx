4f1f27f9bc2857b2339a933ca89089a5ec41ea95
r-santtos <rsanttos.dev@gmail.com>
2022-04-13 17:53:46 -0300

    create test render image

--- __tests__/Image-test.tsx
+++ __tests__/Image-test.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
-import App from "../App";
+import Image from "../src/components/Images";
 
-describe("App", () => {
-  it("renders without crashing", () => {
-    const { container } = render(<App />);
-    expect(container).toBeTruthy();
+describe("Components", () => {
+  it("Should render RN Image", () => {
+    const { getByTestId } = render(<Image />);
+    const image = getByTestId("logo");
+
+    expect(image).toBeTruthy();
+    expect(image.props.source.uri).toBe("https://reactnative.dev/img/tiny_logo.png");
+    expect(image.props.style.width).toBe(200);
+    expect(image.props.style.height).toBe(200);
+    expect(image.props.testID).toBe("logo");
+    expect(image.props.resizeMode).toBe("contain");
+    expect(image.props.accessibilityLabel).toBe("logo");
   });
 });
