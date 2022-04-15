bb1271f67e2c5a7cecb11a885f28d491c9ccfd62
r-santtos <rsanttos.dev@gmail.com>
2022-04-14 22:43:41 -0300

    create component TextInput

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,32 +1,55 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {
   StyleSheet,
-  View,
   Text,
   TextInput,
+  Keyboard,
   KeyboardAvoidingView,
   Platform,
+  TouchableOpacity,
 } from "react-native";
 
 const TextInputComponent = () => {
   const [value, setValue] = useState("");
 
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener("keyboardDidShow", () => {
+      console.log("Keyboard Shown");
+    });
+    const hideSubscription = Keyboard.addListener("keyboardDidHide", () => {
+      console.log("Keyboard Hidden");
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-        <Text style={styles.txt}>TextInput</Text>
-        <TextInput 
-          style={styles.input}
-          placeholder="Test TextInput"
-          placeholderTextColor="#999"
-          keyboardType="default"
-          autoCapitalize="none"
-          autoCorrect={true}
-          value={value}
-          onChangeText={(text) => setValue(text)}
-        />
-      </KeyboardAvoidingView>
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <Text style={styles.txt}>TextInput</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Test TextInput"
+        placeholderTextColor="#999"
+        keyboardType="default"
+        autoCapitalize="none"
+        autoCorrect={true}
+        value={value}
+        onChangeText={(text) => setValue(text)}
+      />
+
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => Keyboard.dismiss()}
+      >
+        <Text style={[styles.txt, {color: '#fff', textTransform: 'uppercase'}]}>enviar</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -47,6 +70,17 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 1,
     borderColor: "#ddd",
+  },
+  btn: {
+    width: "100%",
+    height: 40,
+    backgroundColor: "#4286f4",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    marginTop: 16,
   }
 })
 
